af1a789b9913b28dbc40dfed3962c640af933902
Lochan <98405764+lochanchugh@users.noreply.github.com>
2023-11-11 14:35:07 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,14 +7,25 @@ export default function Index2() {
   const [score, setScore] = useState("Live Score Data...")
   const [batsman, setBatsman] = useState("Batsman Data...")
   const [batsmanrun, setBatsmanrun] = useState("")
+  const [fours, setBatsmanfours] = useState("")
+  const [sixes, setBatsmansixes] = useState("")
+  const [sr, setBatsmansr] = useState("")
   
   const [batsmantwo, setBatsmantwo] = useState("Batsman Data...")
   const [batsmantworun, setBatsmantworun] = useState("")
+  const [batsmantwofours, setBatsmantwofours] = useState("")
+  const [batsmantwosixes, setBatsmantwosixes] = useState("")
+  const [batsmantwosr, setBatsmantwosr] = useState("")
   
   const [bowler, setBowler] = useState("")
   const [bowlerruns, setBowlerruns] = useState("")
   const [bowlerover, setBowlerover] = useState("")
   const [bowlerwickets, setBowlerwickets] = useState("")
+
+  const [recentballs, setRecentballs] = useState("")
+  const [lastwicket, setLastwicket] = useState("")
+  const [partnership, setPartnership] = useState("")
+  const [commentary, setCommentary] = useState("")
   const [runrate, setRunrate] = useState("Fetching Run rate")
   const [update, setUpdate] = useState("match Update")
   const fetchWord = async () => {
@@ -23,13 +34,26 @@ export default function Index2() {
     setTitle(data.title)
     setScore(data.current)
     setBatsman(data.batsman)
-    setBatsmantwo(data.batsmantwo)
     setBatsmanrun(data.batsmanrun)
+    setBatsmanfours(data.fours)
+    setBatsmansixes(data.sixes)
+    setBatsmansr(data.sr)
+    
+    setBatsmantwo(data.batsmantwo)
     setBatsmantworun(data.batsmantworun)
+    setBatsmantwofours(data.batsmantwofours)
+    setBatsmantwosixes(data.batsmantwosixes)
+    setBatsmantwosr(data.batsmantwosr)
+    
     setBowler(data.bowler)
     setBowlerover(data.bowlerover)
     setBowlerruns(data.bowlerruns)
     setBowlerwickets(data.bowlerwickets)
+
+    setRecentballs(data.recentballs)
+    setLastwicket(data.lastwicket)
+    setPartnership(data.partnership)
+    setCommentary(data.commentary)
     setRunrate(data.runrate)
     setUpdate(data.update)
   }
@@ -37,7 +61,7 @@ export default function Index2() {
   if (batsman === "Data Not Found") {
     Displayscore = (
       <p className="text-gray-800 dark:text-gray-100 text-sm">
-        {"🏏 " + title}
+       <h1> {"🏏 " + title}</h1>
         <br /> <br />
         {"📊 " + update}
       </p>
@@ -45,15 +69,15 @@ export default function Index2() {
   } else {
     Displayscore = (
       <p className="text-gray-800 dark:text-gray-100 text-sm">
-        {"🏏 " + title}
+       <h1> {"🏏 " + title} </h1>
         <br />
         <br /> {"📊 " + update}
         <br />
         <br /> {"🔴 " + score}
         <br />
-        <br /> {"🏏 " + batsman} {batsmanrun}
+        <br /> {"🏏 " + batsman} {batsmanrun} {"fours" + fours + "sixes" + sixes + "strike rate" + sr}
 <br />
-<br />{ batsmantwo} {batsmantworun}
+<br />{batsmantwo} {batsmantworun} {"fours" + batsmantwofours + "sixes" + batsmantwosixes + "strike rate" + batsmantwosr}
         <br />
         <br />{" "}
         {"🥎 " +
@@ -67,6 +91,9 @@ export default function Index2() {
           " wicket"}
         <br />
         <br /> {"📉 " + runrate}
+
+
+{"Recentballs" + recentballs + "Last Wicket" + lastwicket + "Partnership" + partnership + "Commentary" + commentary}
       </p>
     )
   }
