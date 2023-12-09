67ad26a5593d05ae5555691604dff20ff6378ce5
Lochan <98405764+lochanchugh@users.noreply.github.com>
2023-12-09 15:34:28 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -194,7 +194,7 @@ setInterval(function(){
 return (
   <div className="min-h-screen bg-blue-50 dark:bg-blue-100 flex flex-col justify-between p-1">
     
-    <div className="bg-yellow-600 rounded-lg p-1 text-center border-solid border-4 border-gray-600 w-1/2">
+    <div className="bg-yellow-400 rounded-lg p-1 text-center border-solid border-4 border-gray-600 w-1/2">
       {Displayscore}
     </div>
   </div>
